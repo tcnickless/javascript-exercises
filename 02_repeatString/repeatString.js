35ca8268aa7b2502448d740f9d30eaee4b09cad9
Tom Nickless <tcnickless@gmail.com>
2023-11-28 23:58:12 +0000

    first attempt at solution in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatNumber) {
+    for (let i=0; i<repeatNumber; i++;) {
+	    console.log(text);
+    }
 };
 
 // Do not edit below this line
