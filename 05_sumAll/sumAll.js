85da43ec9adbbb119976b2f2f50a9e7cc3c0e45e
Tom Nickless <tcnickless@gmail.com>
2023-11-29 17:42:47 +0000

    solution for ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function(firstNumber, lastNumber) {
+const sumAll = function(numberX, numberY) {
     let endSum = 0;
+
+    // throw error if input is out of bounds (negative)
+    if ((numberX || numberY) < 0) {
+        return 'ERROR';
+    }
+    // throw error if types are not INT
+    if (!Number.isInteger(numberX) || !Number.isInteger(numberY)) {
+        return 'ERROR';
+    }
+
+    // sanitise input for iterating over
+    if (numberX > numberY) {
+	firstNumber = numberY;
+	lastNumber = numberX;
+    } else {
+	firstNumber = numberX;
+	lastNumber = numberY;
+    }
 
     for (i = firstNumber; i <= lastNumber; i++) {
         endSum += i;
