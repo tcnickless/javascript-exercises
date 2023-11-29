f3f2ecbe1d023d475c2b18ce6b16215e5f5b0a5d
Tom Nickless <tcnickless@gmail.com>
2023-11-29 10:34:02 +0000

    corrected syntax for join function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,8 +2,8 @@ const reverseString = function(text) {
     let splitString = text.split("");
     let reversedString = "";
 
-    reversedArray = splitString.reverse();
-    reversedString = reversedArray.join();
+    let reversedArray = splitString.reverse();
+    reversedString = reversedArray.join("");
     return reversedString;
 };
 
