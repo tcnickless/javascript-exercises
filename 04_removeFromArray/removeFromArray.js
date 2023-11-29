ee44ab3793a3cd97b0eb6804e1d46744e4485c06
Tom Nickless <tcnickless@gmail.com>
2023-11-29 16:31:04 +0000

    solution #2 for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,10 +2,10 @@ const removeFromArray = function(inputArray, valueToRemove) {
     let outPutArray = [];
     for (i = 0; i < inputArray.length; i++) {
         if (inputArray[i] === valueToRemove) {
-            outputArray = inputArray.splice(i);
+            inputArray.splice(i, 1);
         }
     }
-    return outputArray;
+    return inputArray;
 };
 
 // Do not edit below this line
