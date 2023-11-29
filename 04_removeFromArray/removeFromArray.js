cc240c8f18140fc7adca81aee6389a39dda4a3a2
Tom Nickless <tcnickless@gmail.com>
2023-11-29 16:02:05 +0000

    solution #1 for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, indexToRemove) {
+    return inputArray.splice(indexToRemove, 1);
 };
 
 // Do not edit below this line
