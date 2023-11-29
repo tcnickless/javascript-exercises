bf4a1d2d427069ade2bb2c92e001dd29251af3ad
Tom Nickless <tcnickless@gmail.com>
2023-11-29 16:59:43 +0000

    solution for full excercise, with comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,15 @@
 const removeFromArray = function(inputArray) {
+    // for every argument, excluding the initial array
     for (j = 1; j <= arguments.length; j++) {
-        for (i = 0; i < inputArray.length; i++) {   
-                if (inputArray[i] === arguments[j]) {
-                    inputArray.splice(i, 1);
-                }
+        // for every element of inputArray
+        for (i = 0; i < inputArray.length; i++) {
+            // check if argument J matches the array element
+            // if a match, remove the element
+            if (inputArray[i] === arguments[j]) {
+                inputArray.splice(i, 1);
             }
         }
+    }
     return inputArray;
 };
 
