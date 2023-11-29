5dd6f5a6408c6bb9309cb251afc3f7fba6bf5256
Tom Nickless <tcnickless@gmail.com>
2023-11-29 18:37:04 +0000

    added solution for ex6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+	// check for leap century and return true, but return false if a regular century
+	if ( ((year % 100) === 0)  &&  ((year % 400) === 0)  ) {
+		return true;
+	} else if ((year % 100) === 0) {
+		return false;
+	}
+
+	// check for regular leapness
+	if ((year % 4) === 0) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
